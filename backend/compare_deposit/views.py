7797585c726ef914812c2f8cbe208e826e99cd60
GwangSu <kimgs8882005@gmail.com>
2024-05-09 17:49:57 +0900

    0509_backend_api_settings

--- backend/compare_deposit/views.py
+++ backend/compare_deposit/views.py
@@ -1,3 +1,20 @@
+import requests
 from django.shortcuts import render
-
+from django.conf import settings
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 # Create your views here.
+API_KEY = settings.API_KEY
+
+BASE_URL = 'http://finlife.fss.or.kr/finlifeapi/'
+
+@api_view(['GET'])
+def api_test(request):
+    URL = BASE_URL + 'depositProductsSearch.json'
+    params = {
+        'auth': settings.API_KEY,
+        'topFinGrpNo' : '020000',
+        'pageNo':1
+    }
+    respones = requests.get(URL,params=params).json()
+    return JsonResponse({'respones':respones})
